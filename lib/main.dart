8b5f094b31c0f2b62ba8c535f7bfe65f75ead8c5
Tosin8 <tosinezekiel8@gmail.com>
2022-07-07 10:45:01 +0100

    updating the main.dart page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      title: 'Product Listing'; 
-    home: Product_List()));
+  runApp(const MaterialApp(title: 'Product Listing', home: Product_List()));
 }
 
+// ignore: camel_case_types
+class Product_List extends StatefulWidget {
+  const Product_List({Key? key}) : super(key: key);
+
+  @override
+  State<Product_List> createState() => _Product_ListState();
+}
+
+// ignore: camel_case_types
+class _Product_ListState extends State<Product_List> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        const Text('Product Listing'),
+      ),
+      body: ListView(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween, 
+        children: const [
+          ProductBox(), 
+          ProductBox(),
+        ]),
+    );
+  }
+}
+
+class ProductBox extends StatefulWidget {
+  const ProductBox({Key? key}) : super(key: key);
+
+  @override
+  State<ProductBox> createState() => _ProductBoxState();
+}
+
+class _ProductBoxState extends State<ProductBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      child: Card(
+        child: Row(
+          child: Image.asset('assets/images/' + images), 
+          Expanded(
+            child:Column(
+
+            ) ,)
+        )
+          
+          
+        ),
+      ),
+    )
+    
+  }
+}

db6e418fd5e00bf5ee64e45dcdc6e67604650c6a
Tosin8 <tosinezekiel8@gmail.com>
2022-07-08 23:37:26 +0100

    Adding the product file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:product_listing/extension/rating.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:scoped_model/scoped_model.dart';
 
 void main() {
   runApp(const MaterialApp(

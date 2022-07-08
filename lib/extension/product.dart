ef7379f3df1ade3234449d89831dd9a71154331e
Tosin8 <tosinezekiel8@gmail.com>
2022-07-08 23:53:09 +0100

    Update product.dart

--- lib/extension/product.dart
+++ lib/extension/product.dart
@@ -32,3 +32,54 @@ class Product extends Model {
     notifyListeners();
   }
 }
+
+
+static List<Product> getProduct(){
+  List<Product> items = <Product>[]; 
+
+  items.add(Product(
+    'Beats Pro 14',
+    'Light for Outdoor Recreation',
+    90,
+    'beats-pro-14.jpg',
+    0,
+  ));
+  items.add(Product(
+    'Beats EP',
+    'Best for Professional editing',
+    109,
+    'Beats-EP-On-Ear-Headphones.jpg',
+    0,
+  ));
+
+  items.add(Product(
+    'Beats v.14',
+    'Noise-cancelling headphones',
+    176,
+    '2.jpg',
+    0,
+  ));
+  items.add(Product(
+    'JBL Headbeat 12',
+    'Noise-cancelling headphones',
+    200,
+    'jbl-tune-500bt-wireless-on-ear-headphones-black.jpg',
+    0,
+  ));
+
+  items.add(Product(
+    'Beats Pro 14',
+    'Noise-cancelling headphones',
+    349,
+    'MRJ82.jpg',
+    0,
+  ));
+  
+  items.add(Product(
+    'Beats Pro 14',
+    'Noise-cancelling headphones',
+    215,
+    '1.jpg',
+    0,
+  ));
+

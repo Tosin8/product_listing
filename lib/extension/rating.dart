7971636b92d69ebd642ba9bdc8194458ea17b79c
Tosin8 <tosinezekiel8@gmail.com>
2022-07-08 21:51:15 +0100

    Update rating.dart

--- lib/extension/rating.dart
+++ lib/extension/rating.dart
@@ -1,2 +1,63 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
 
+class RatingBox extends StatefulWidget {
+  const RatingBox({Key? key}) : super(key: key);
+
+  @override
+  State<RatingBox> createState() => _RatingBoxState();
+}
+
+class _RatingBoxState extends State<RatingBox> {
+  int _rating = 0;
+
+  void _setRatingAsOne() {
+    setState(() {
+      _rating = 1;
+    });
+  }
+
+  void _setRatingAsTwo() {
+    setState(() {
+      _rating = 2;
+    });
+  }
+
+  void _setRatingAsThree() {
+    setState(() {
+      _rating = 3;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: no_leading_underscores_for_local_identifiers
+    double _size = 20;
+    // ignore: avoid_print
+    print(_rating);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 1
+                ? Icon(
+                    Icons.star,
+                    size: _size,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: _size,
+                  )),
+            color: Colors.red,
+            onPressed: _setRatingAsOne,
+            iconSize: _size,
+          ),
+        )
+      ],
+    );
+  }
+}
